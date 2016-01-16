dee76ed6a1e6e1eb073297df8457fd598802b68e
Jules <yanpanpan@Hymans-Work-Mac.local>
2016-01-16 17:20:10 +0800

    创建验证码工程

---
+++ Search_Captcha/Search_Captcha/CaptchaVerifyView.swift
@@ -0,0 +1,60 @@
+//
+//  CaptchaVerifyView.swift
+//  Search_Captcha
+//
+//  Created by Jules on 16/1/16.
+//  Copyright © 2016年 WeCar. All rights reserved.
+//
+
+import UIKit
+
+class CaptchaVerifyView: UIView {
+
+
+}
+
+
+class CaptchaView: UIView {
+    
+    // MARK: - Assist Variable
+    
+    
+    
+    // MARK: - Load
+    
+    private init() {
+        super.init(frame: CGRectZero)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    
+    override func drawRect(rect: CGRect) {
+        
+//        self.backgroundColor = UIColor.
+        
+    }
+    
+    deinit {
+    }
+    
+    
+    // MARK: - Private Method
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Outer Interface
+    //==============对外开放接口=====================
+    
+
+}
+
+
